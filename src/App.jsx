33e03faa1cd6dc3f9aa921ab2010acbbd5912a15
Nicolas SILVY <silvynicolas@gmail.com>
2025-03-23 23:11:19 +0100

    added main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-function App() {
-  const [count, setCount] = useState(0)
+import TodoList from "./components/TodoList"
+import TodoInput from "./components/TodoInput"
+import {useState} from "react"
 
+function App() {
+
+  const [todos, setTodos] = useState([
+    'Go to the gym',
+    'Eat more fruits',
+    'Learn React'
+  ])
+
+  function handleAddTodos(newTodo){
+    const newTodoList = [...todos,newTodo]
+    setTodos(newTodoList)
+  }
+  
   return (
     <>
-      
+     <TodoInput handleAddTodos={handleAddTodos}/>
+     <TodoList todos={todos}/>
     </>
   )
 }
---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function TodoCard(props) {
+  const {children}=props
+  return (
+    <li className="todoItem">
+      {children}
+      <div className="actionsContainer">
+        <i className="fa-regular fa-pen-to-square"></i>
+        <i className="fa-regular fa-trash-can"></i>
+      </div>
+    </li>
+  );
+}
